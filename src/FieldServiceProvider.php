b136c99aefe432027c86f7ad1b6ad1d227ace274
Gabriel Diez <gabriel.diez@rosariosoftware.com>
2021-08-31 11:38:09 -0300

    Initial commit

---
+++ src/FieldServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Diezg\Collapsable;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class FieldServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::script('collapsible', __DIR__.'/../dist/js/field.js');
+            Nova::style('collapsible', __DIR__.'/../dist/css/field.css');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/HasManyCollapsable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Diezg\Collapsable;
+
+use Laravel\Nova\Fields\HasMany;
+
+class HasManyCollapsable extends HasMany
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'collapsible';
+
+    public function __construct($name, $attribute = null, $resource = null)
+    {
+        $this->withMeta([
+            'show' => __('Show') . ' ' . $name,
+            'hide' => __('Hide') . ' ' . $name
+        ]);
+        parent::__construct($name, $attribute, $resource);
+    }
+
+    /**
+     * Set the texts button.
+     *
+     * @param  string  $show
+     * @param  string  $hide
+     * @return $this
+     */
+    public function buttonText(string $show, string $hide)
+    {
+        return $this->withMeta([
+            'show' => $show,
+            'hide' => $hide
+        ]);
+    }
+}
